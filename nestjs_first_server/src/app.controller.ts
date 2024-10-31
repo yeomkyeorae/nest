4bfbb3ab178b4a0056073178b690f56059499d22
Kyeorae Yeom <duarufp06@gmail.com>
2024-10-31 22:49:43 +0900

    nextjs_first_server 내 .git 제거

---
+++ nestjs_first_server/src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHome() {
+    return 'Home page';
+  }
+
+  @Get('post')
+  getPost() {
+    return 'Post page';
+  }
+
+  @Get('user')
+  getUser() {
+    return 'User page';
+  }
+}
