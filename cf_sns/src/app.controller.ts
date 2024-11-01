03bfb79027fce7fa6208b78b98908849f4efe07b
Kyeorae Yeom <duarufp06@gmail.com>
2024-11-01 22:15:00 +0900

    post JSON 변환

--- cf_sns/src/app.controller.ts
+++ cf_sns/src/app.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'yeomkyeorae',
+      title: 'it is me',
+      content: 'Who are you?',
+      likeCount: 10,
+      commentCount: 5,
+    };
   }
 }
