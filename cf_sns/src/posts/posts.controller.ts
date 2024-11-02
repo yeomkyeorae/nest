0819134dfe35c43e492897b9e9127e705aa6f406
Kyeorae Yeom <duarufp06@gmail.com>
2024-11-02 17:38:44 +0900

    GET posts 구현

--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +10,53 @@ interface Post {
   commentCount: number;
 }
 
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'mourinho',
+    title: 'this is soccer',
+    content: 'do it',
+    likeCount: 10000,
+    commentCount: 90,
+  },
+  {
+    id: 2,
+    author: 'simeone',
+    title: 'this is not soccer',
+    content: 'just do it',
+    likeCount: 11000,
+    commentCount: 95,
+  },
+  {
+    id: 3,
+    author: 'kompany',
+    title: 'this is not football',
+    content: 'just do it like me',
+    likeCount: 12000,
+    commentCount: 92,
+  },
+];
+
 @Controller('posts') // 첫 인자는 기존 path에 접두어 붙이는 역할
 export class PostsController {
   constructor(private readonly appService: PostsService) {}
 
+  // 1) GET /posts
+  // 모든 post 가져오기
   @Get()
-  getPost(): Post {
-    return {
-      author: 'yeomkyeorae',
-      title: 'it is me',
-      content: 'Who are you?',
-      likeCount: 10,
-      commentCount: 5,
-    };
+  getPosts() {
+    return posts;
   }
+
+  // 2) GEP /posts/:id
+  // id에 해당하는 post 가져오기
+
+  // 3) POST /posts
+  // post 생성
+
+  // 4) PUT /posts/:id
+  // id에 해당하는 post 변경
+
+  // 5) DELETE /posts/:d
+  // id에 해댕하는 post 삭제
 }
