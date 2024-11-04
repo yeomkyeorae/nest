bd3e0f2c3f28c94b10626ad72a7e0eb80e49da85
Kyeorae Yeom <duarufp06@gmail.com>
2024-11-04 22:20:28 +0900

    controller에서 service로의 로직 이동

--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -3,70 +3,28 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'mourinho',
-    title: 'this is soccer',
-    content: 'do it',
-    likeCount: 10000,
-    commentCount: 90,
-  },
-  {
-    id: 2,
-    author: 'simeone',
-    title: 'this is not soccer',
-    content: 'just do it',
-    likeCount: 11000,
-    commentCount: 95,
-  },
-  {
-    id: 3,
-    author: 'kompany',
-    title: 'this is not football',
-    content: 'just do it like me',
-    likeCount: 12000,
-    commentCount: 92,
-  },
-];
-
 @Controller('posts') // 첫 인자는 기존 path에 접두어 붙이는 역할
 export class PostsController {
-  constructor(private readonly appService: PostsService) {}
+  constructor(private readonly postsService: PostsService) {}
 
   // 1) GET /posts
   // 모든 post 가져오기
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   // 2) GEP /posts/:id
   // id에 해당하는 post 가져오기
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   // 3) POST /posts
@@ -77,18 +35,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-
-    posts = [...posts, post];
-
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   // 4) PUT /posts/:id
@@ -100,40 +47,13 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    if (author) {
-      post.author = author;
-    }
-
-    if (title) {
-      post.title = title;
-    }
-
-    if (content) {
-      post.content = content;
-    }
-
-    posts = posts.map((prevPost) => (post.id === +id ? post : prevPost));
-
-    return post;
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   // 5) DELETE /posts/:d
   // id에 해댕하는 post 삭제
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    posts = posts.filter((post) => post.id !== +id);
-    return id;
+    return this.postsService.deletePost(+id);
   }
 }
--- cf_sns/src/posts/posts.service.ts
+++ cf_sns/src/posts/posts.service.ts
@@ -1,4 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'mourinho',
+    title: 'this is soccer',
+    content: 'do it',
+    likeCount: 10000,
+    commentCount: 90,
+  },
+  {
+    id: 2,
+    author: 'simeone',
+    title: 'this is not soccer',
+    content: 'just do it',
+    likeCount: 11000,
+    commentCount: 95,
+  },
+  {
+    id: 3,
+    author: 'kompany',
+    title: 'this is not football',
+    content: 'just do it like me',
+    likeCount: 12000,
+    commentCount: 92,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+
+    return post;
+  }
+
+  updatePost(postId: number, author: string, title: string, content: string) {
+    const post = posts.find((post) => post.id === postId);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (post.id === postId ? post : prevPost));
+
+    return post;
+  }
+
+  deletePost(postId: number) {
+    const post = posts.find((post) => post.id === postId);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter((post) => post.id !== postId);
+    return postId;
+  }
+}
