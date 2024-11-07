4f0950923ee0abbf9b9625db8c1c1f02c5feede7
Kyeorae Yeom <duarufp06@gmail.com>
2024-11-07 21:12:31 +0900

    typeorm for postgres 설정

--- cf_sns/src/app.module.ts
+++ cf_sns/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // 데이터베이스 타입
+      host: '127.0.0.1',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
