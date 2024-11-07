a2bdec6aadf8e855d184224967e8511f0eaffeda
Kyeorae Yeom <duarufp06@gmail.com>
2024-11-07 22:10:24 +0900

    create & save 함수

--- cf_sns/src/posts/posts.service.ts
+++ cf_sns/src/posts/posts.service.ts
@@ -65,19 +65,21 @@ export class PostsService {
     return post;
   }
 
-  createPost(author: string, title: string, content: string) {
-    const post = {
-      id: posts[posts.length - 1].id + 1,
+  async createPost(author: string, title: string, content: string) {
+    // create 메소드 -> 저장할 객체를 생성
+    // save 메소드 -> create 메소드에서 생성한 객체를 저장
+
+    const post = this.postsRepository.create({
       author,
       title,
       content,
       likeCount: 0,
       commentCount: 0,
-    };
+    });
 
-    posts = [...posts, post];
+    const newPost = await this.postsRepository.save(post);
 
-    return post;
+    return newPost;
   }
 
   updatePost(postId: number, author: string, title: string, content: string) {
