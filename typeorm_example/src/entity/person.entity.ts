1d6741d58d4b8baa4756cf06e9b2949496dfb0de
Kyeorae Yeom <duarufp06@gmail.com>
2024-11-12 18:07:20 +0900

    entity embedding

---
+++ typeorm_example/src/entity/person.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Name {
+  @Column()
+  first: string;
+
+  @Column()
+  last: string;
+}
+
+@Entity()
+export class StudentModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // Entity Embedding by Name
+  @Column(() => Name)
+  name: Name;
+
+  @Column()
+  class: string;
+}
+
+@Entity()
+export class TeacherModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column(() => Name)
+  name: Name;
+
+  @Column()
+  salay: number;
+}
