c3d2d8754e5dfa929ec1c7ec3c378f1f79323577
Kyeorae Yeom <duarufp06@gmail.com>
2024-11-20 22:49:51 +0900

    many to many relationship example

--- typeorm_example/src/app.controller.ts
+++ typeorm_example/src/app.controller.ts
@@ -4,6 +4,7 @@ import { Role, UserModel } from './entity/user.entity';
 import { Repository } from 'typeorm';
 import { ProfileModel } from './entity/profile.entity';
 import { PostModel } from './entity/post.entity';
+import { TagModel } from './entity/tag.entity';
 
 @Controller()
 export class AppController {
@@ -14,6 +15,8 @@ export class AppController {
     private readonly profileRepository: Repository<ProfileModel>,
     @InjectRepository(PostModel)
     private readonly postRepository: Repository<PostModel>,
+    @InjectRepository(TagModel)
+    private readonly tagRepository: Repository<TagModel>,
   ) {}
 
   @Post('users')
@@ -79,5 +82,47 @@ export class AppController {
     });
 
     return user;
+  }
+
+  @Post('posts/tags')
+  async createPostsTags() {
+    const post1 = await this.postRepository.save({
+      title: 'nextjs',
+    });
+    const post2 = await this.postRepository.save({
+      title: 'nestjs',
+    });
+    const tag1 = await this.tagRepository.save({
+      name: 'javascript',
+      posts: [post1, post2],
+    });
+    const tag2 = await this.tagRepository.save({
+      name: 'typescript',
+      posts: [post1],
+    });
+    await this.postRepository.save({
+      title: 'docker',
+      tags: [tag1, tag2],
+    });
+
+    return true;
+  }
+
+  @Get('posts')
+  getPosts() {
+    return this.postRepository.find({
+      relations: {
+        tags: true,
+      },
+    });
+  }
+
+  @Get('tags')
+  getTags() {
+    return this.tagRepository.find({
+      relations: {
+        posts: true,
+      },
+    });
   }
 }
