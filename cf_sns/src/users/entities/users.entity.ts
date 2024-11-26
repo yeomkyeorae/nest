c9614a5a523a8dbc18c202dd7c923e262d5ab7ff
Kyeorae Yeom <duarufp06@gmail.com>
2024-11-26 21:50:29 +0900

    Column options

--- cf_sns/src/users/entities/users.entity.ts
+++ cf_sns/src/users/entities/users.entity.ts
@@ -5,10 +5,18 @@ export class UsersModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    length: 20,
+    unique: true,
+  })
+  // 1) 길이가 20을 넘지 않을 것
+  // 2) 유일무이한 값이 될 것
   nickname: string;
 
-  @Column()
+  @Column({
+    unique: true,
+  })
+  // 1) 유일무이한 값이 될 것
   email: string;
 
   @Column()
