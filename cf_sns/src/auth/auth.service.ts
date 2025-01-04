0ab41c19b6b33db713fd559dbd953544cca96873
Kyeorae Yeom <duarufp06@gmail.com>
2025-01-04 12:12:14 +0900

    signToken 함수 작성성

--- cf_sns/src/auth/auth.service.ts
+++ cf_sns/src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { JWT_SECRET } from './const/auth.const';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
   /**
    * 만드는 기능
    *
@@ -27,4 +31,23 @@ export class AuthService {
    *  3. 모두 통과되면 사용자 정보 반환
    *  4. loginWithEmail에서 반환된 데이터를 기반으로 토큰 반환
    */
+
+  /**
+   * payload에 들어갈 정보
+   * 1) email
+   * 2) sub -> ID
+   * 3) type: 'access' | 'refresh'
+   */
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
 }
