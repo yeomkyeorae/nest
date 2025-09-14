b13ee017e4cc3d15144090faccbb73008d3f3eb0
Kyeorae Yeom <duarufp06@gmail.com>
2025-09-14 23:25:08 +0900

    여러 개의 파이프 동시에 사용하기

--- cf_sns/src/auth/pipe/password.pipe.ts
+++ cf_sns/src/auth/pipe/password.pipe.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   // ArgumentMetadata,
   BadRequestException,
+  ArgumentMetadata,
 } from '@nestjs/common';
 
 @Injectable()
@@ -13,6 +14,40 @@ export class PasswordPipe implements PipeTransform {
       throw new BadRequestException('비밀번호는 8자 이하로 입력해 주세요.');
     }
 
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MaxLengthPipe implements PipeTransform {
+  constructor(
+    private readonly length: number,
+    private readonly subject: string,
+  ) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length > this.length) {
+      throw new BadRequestException(
+        `${this.subject} 최대 길이는 ${this.length}입니다.`,
+      );
+    }
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MinLengthPipe implements PipeTransform {
+  constructor(
+    private readonly length: number,
+    private readonly subject: string,
+  ) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length < this.length) {
+      throw new BadRequestException(
+        `${this.subject} 최소 길이는 ${this.length}입니다.`,
+      );
+    }
     return value.toString();
   }
 }
--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  DefaultValuePipe,
   Delete,
   Get,
   Param,
@@ -36,7 +35,6 @@ export class PostsController {
     @Body('author') authorId: number,
     @Body('title') title: string,
     @Body('content') content: string,
-    @Body('isPublic', new DefaultValuePipe(true)) isPublic: boolean,
   ) {
     return this.postsService.createPost(authorId, title, content);
   }
