d1b0092de0b1026a76aaf906f299b354d338b110
Kyeorae Yeom <duarufp06@gmail.com>
2025-10-01 22:54:13 +0900

    Class Validator 에러 메시지 변경

--- cf_sns/src/posts/dto/create-post.dto.ts
+++ cf_sns/src/posts/dto/create-post.dto.ts
@@ -1,9 +1,13 @@
 import { IsString } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
+  @IsString({
+    message: '제목은 문자열이어야 합니다.',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: '내용은 문자열이어야 합니다.',
+  })
   content: string;
 }
