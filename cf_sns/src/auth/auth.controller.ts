b47d586d5510516a7f356c43ae0167206ac32c8d
Kyeorae Yeom <duarufp06@gmail.com>
2025-10-13 22:40:11 +0900

    Length Annotation, Email Annotation

--- cf_sns/src/auth/auth.controller.ts
+++ cf_sns/src/auth/auth.controller.ts
@@ -1,15 +1,16 @@
 import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import {
-  MaxLengthPipe,
-  MinLengthPipe,
-  // PasswordPipe,
-} from './pipe/password.pipe';
+// import {
+//   MaxLengthPipe,
+//   MinLengthPipe,
+//   // PasswordPipe,
+// } from './pipe/password.pipe';
 import { BasicTokenGuard } from './guard/basic-token.guard';
 import {
   // AccessTokenGuard,
   RefreshTokenGuard,
 } from './guard/bearer-token.guard';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -55,20 +56,7 @@ export class AuthController {
   }
 
   @Post('register/email')
-  postRegisterEmail(
-    @Body('nickname') nickname: string,
-    @Body('email') email: string,
-    @Body(
-      'password',
-      new MaxLengthPipe(8, '비밀번호'),
-      new MinLengthPipe(3, '비밀번호'),
-    )
-    password: string,
-  ) {
-    return this.authService.registerWithEmail({
-      nickname,
-      email,
-      password,
-    });
+  postRegisterEmail(@Body() body: RegisterUserDto) {
+    return this.authService.registerWithEmail(body);
   }
 }
--- cf_sns/src/users/entities/users.entity.ts
+++ cf_sns/src/users/entities/users.entity.ts
@@ -2,6 +2,7 @@ import { Column, Entity, OneToMany } from 'typeorm';
 import { RolesEnum } from '../const/roles.const';
 import { PostsModel } from 'src/posts/entities/posts.entity';
 import { BaseModel } from 'src/common/entity/base.entity';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -11,15 +12,21 @@ export class UsersModel extends BaseModel {
   })
   // 1) 길이가 20을 넘지 않을 것
   // 2) 유일무이한 값이 될 것
+  @IsString()
+  @Length(1, 20, { message: '닉네임은 1~20자 사이로 입력해주세요' })
   nickname: string;
 
   @Column({
     unique: true,
   })
   // 1) 유일무이한 값이 될 것
+  @IsString()
+  @IsEmail()
   email: string;
 
   @Column()
+  @IsString()
+  @Length(3, 8)
   password: string;
 
   @Column({
