430cda71da585b85f8bd8dd0ac3d4ce91a02aae7
Kyeorae Yeom <duarufp06@gmail.com>
2025-10-15 22:57:23 +0900

    Expose Annotation 적용

--- cf_sns/src/users/entities/users.entity.ts
+++ cf_sns/src/users/entities/users.entity.ts
@@ -6,7 +6,7 @@ import { IsEmail, IsString, Length } from 'class-validator';
 import { lengthValidationMessage } from 'src/common/validation-message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { emailValidationMessage } from 'src/common/validation-message/email-validation.message';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -21,6 +21,11 @@ export class UsersModel extends BaseModel {
     message: lengthValidationMessage,
   })
   nickname: string;
+
+  @Expose()
+  get nicknameAndEmail() {
+    return this.nickname + '/' + this.email;
+  }
 
   @Column({
     unique: true,
