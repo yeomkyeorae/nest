f1c45cb1ffc0eba01d935bcd9299bca4141ba440
Kyeorae Yeom <duarufp06@gmail.com>
2025-10-15 22:55:14 +0900

    ClassSerializer AppModule 적용

--- cf_sns/src/users/users.controller.ts
+++ cf_sns/src/users/users.controller.ts
@@ -1,9 +1,4 @@
-import {
-  ClassSerializerInterceptor,
-  Controller,
-  Get,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -11,7 +6,6 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @UseInterceptors(ClassSerializerInterceptor)
   /**
    * serialization -> 직렬화 -> 현재 시스템에서 사용되는 (NestJS) 데이터의 구조를 다른 시스템에서도 쉽게 사용할 수 있는 포맷으로 변환
    * -> class의 object에서 JSON 포맷으로 변환
